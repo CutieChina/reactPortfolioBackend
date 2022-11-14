ec838f203bfc734a72bfbd727caf8ccb0645bdf9
China Jameson <csjameson008@gmail.com>
2022-11-14 18:01:16 -0500

    still trying

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://china:tPPnVwCdu6G42az@cluster0.xyjfdcf.mongodb.net/?retryWrites=true&w=majority";
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/db', (req, res)=>{
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.connect(err => {
+    const collection = client.db("sample_mflix").collection("comments");
+     const query = { name: 'Mercedes Tyler' };
+     const comments = collection.findOne(query);
+     res.send(comments);
+  });
 })
+
+app.listen(port, () => {
+
+console.log(`Example app listening on port ${port}`)
+})
+
