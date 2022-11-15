30e0ceae03847a6e292bb8a156339e9c0f2fcabb
China Jameson <csjameson008@gmail.com>
2022-11-15 15:16:50 -0500

    connected to frontend, receiving data

--- App.js
+++ App.js
@@ -2,21 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3001
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://china:tPPnVwCdu6G42az@cluster0.xyjfdcf.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://china:XcvwPz3UVM97q4t@cluster0.xyjfdcf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/db', (req, res)=>{
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  client.connect(err => {
-    const collection = client.db("sample_mflix").collection("comments");
-     const query = { name: 'Mercedes Tyler' };
-     const comments = collection.findOne(query);
-     res.send(comments);
-  });
+app.get('/db', async (req, res)=>{
+  await client.connect();
+  const collection = client.db("sample_mflix").collection("comments");
+   const query = { name: 'Mercedes Tyler' };
+   const comments = await collection.findOne(query);
+   res.send(comments);
 })
 
 app.listen(port, () => {
